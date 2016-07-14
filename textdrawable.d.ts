98e53835ce7f404e1a55b7f8543965b9c3c7e8bc
Osei Fortune <fortune.osei@yahoo.com>
2016-07-14 02:47:57 -0400

    Clean up & added typings

---
+++ textdrawable.d.ts
@@ -0,0 +1,13 @@
+import * as view from 'ui/core/view';
+export declare class TextDrawable extends view.View {
+    text: string;
+    type: string;
+    textColor: string;
+    useFont: string;
+    fontSize: number;
+    toUpperCase: boolean;
+    withBorder: number;
+    bold: boolean;
+    bgColor: string;
+    radius: number;
+}
